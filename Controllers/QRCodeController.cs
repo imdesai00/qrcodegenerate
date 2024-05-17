55b588e17d352d66c452d1c2b2b2a8daa60bbf9f
imdesai00 <harsh.hd.desai00@gmail.com>
2024-05-17 10:35:55 +0530

    first commit

---
+++ Controllers/QRCodeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder; // Make sure you have this using directive
+using System;
+using System.Drawing;
+using System.IO;
+using static QRCoder.PayloadGenerator;
+using System.Reflection;
+using qrimage.Model;
+using System.Text;
+using Microsoft.AspNetCore.Hosting.Server;
+
+namespace qrimage.Controllers // Make sure this namespace matches your project namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QRCodeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GenerateQRCode(QRCodeModel model)
+        {
+
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            {
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(model.QRCodeText, QRCodeGenerator.ECCLevel.Q);
+                BitmapByteQRCode qrCode = new(qrCodeData);
+                string base64String = Convert.ToBase64String(qrCode.GetGraphic(20));
+                byte[] imageBytes = Convert.FromBase64String(base64String);
+                string filePath = "QRCodeImage.png";
+                System.IO.File.WriteAllBytes(filePath, imageBytes);
+                return Ok($"QR code image saved as: {filePath}");
+            }
+        }
+    }
+}
---
+++ Model/QRCodeModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace qrimage.Model
+{
+    public class QRCodeModel
+    {
+        public string QRCodeText { get; set; }
+    }
+}
